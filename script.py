1430e134c3098e1570b1d8fd2664064462bd854c
marta.pancaldi <marta.pancaldi@resourcify.de>
2023-04-25 15:28:42 +0200

    Add toggle_plug function

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
 import os
+import time
 
 from dotenv import load_dotenv
 from tapo_plug import tapoPlugApi
+
+PLUG_DEVICE = "Router smart plug"
+TIME_OFF = 2
 
 
 def main():
@@ -10,11 +14,24 @@ def main():
     device = {
         "tapoIp": os.environ["tapoIp"],
         "tapoEmail": os.environ["tapoEmail"],
-        "tapoPassword": os.environ["tapoPassword"]
+        "tapoPassword": os.environ["tapoPassword"],
+        "nickname": PLUG_DEVICE
     }
+
+    toggle_plug(device)
 
     response = tapoPlugApi.getDeviceInfo(device)
     print(response)
+
+
+def toggle_plug(device):
+    print("* %s turned off" % PLUG_DEVICE)
+    tapoPlugApi.plugOff(device)
+
+    time.sleep(TIME_OFF)
+
+    print("* %s turned back on after %s seconds" % (PLUG_DEVICE, TIME_OFF))
+    tapoPlugApi.plugOn(device)
 
 
 if __name__ == '__main__':
